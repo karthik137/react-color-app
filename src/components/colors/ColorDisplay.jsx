2ea9e942d5ea0e7bf60b9d87a383aeeeac0d8781
karthik <raokarthikg@gmail.com>
2020-04-25 21:40:13 +0530

    Initial commit

---
+++ src/components/colors/ColorDisplay.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class ColorDisplay extends Component{ 
+
+    render(){
+
+        return(
+            <div className="colorDisplay">
+                This is color display
+            </div>
+        );
+    }
+
+
+}
+
+
+export default ColorDisplay
